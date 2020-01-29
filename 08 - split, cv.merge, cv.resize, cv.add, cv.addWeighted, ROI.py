adb2e5e8782eb8f9fdfff3195f694d521c69111c
raselkhondoker <mrrasel922@gmail.com>
2020-01-29 11:50:32 +0600

    Add some new files

---
+++ 08 - split, cv.merge, cv.resize, cv.add, cv.addWeighted, ROI.py
@@ -0,0 +1,31 @@
+import cv2
+
+img = cv2.imread("messi5.jpg")
+img2 = cv2.imread("Logo.png")
+
+
+print(img.shape)
+print(img.size)
+print(img.dtype)
+
+
+b,g,r = cv2.split(img)
+img = cv2.merge((b,g,r))
+
+ball = img[280:340, 330:390]
+img[273:333, 100:160] = ball
+
+# Each image must be same size
+
+img = cv2.resize(img, (512, 512))
+img2 = cv2.resize(img2, (512, 512))
+
+
+
+#dst = cv2.add(img, img2)
+dst = cv2.addWeighted(img, .3, img2, .9, 0)
+
+cv2.imshow("Image",dst)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
